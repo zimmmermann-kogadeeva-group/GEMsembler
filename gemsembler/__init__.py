3051f87d0eb190d1ccb3c2226136f73b3edd2f4a
Bartosz Bartmanski <bartoszbartmanski@gmail.com>
2023-12-24 19:25:28 +0000

    Bump version 0.2.1 -> 0.2.2

--- gemsembler/__init__.py
+++ gemsembler/__init__.py
@@ -2,4 +2,4 @@ from .gathering import GatheredModels, load_sbml_model
 from .creation import read_supermodel_from_pkl
 from .anticreation import get_model_of_interest
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
