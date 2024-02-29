fb2848a989b3621c31024cb27715bf17916f2b27
Bartosz Bartmanski <bartoszbartmanski@gmail.com>
2024-02-29 16:34:08 +0100

    Bump version 0.4.2 -> 0.5.0

--- src/gemsembler/__init__.py
+++ src/gemsembler/__init__.py
@@ -5,7 +5,7 @@ from .creation import read_supermodel_from_pkl
 from .data import BU, LP
 from .gathering import GatheredModels, load_sbml_model
 
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 
 lp_example = [
     dict(
