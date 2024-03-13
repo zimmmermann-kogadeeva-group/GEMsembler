be8758991ca2a630193fe0014de82fce4977512f
Elena Matveishina <elena.matveishina@embl.de>
2024-03-13 14:48:44 +0100

    save separate shortest paths file in pathfinding

--- src/gemsembler/pathsfinding.py
+++ src/gemsembler/pathsfinding.py
@@ -23,7 +23,7 @@ def tmp_cwd():
         chdir(oldpwd)
 
 
-def write_mq_data(mq_data: dict, filename: str):
+def write_mq_data(mq_data: dict, filename: Path):
     """
     Function to save output from metquest in hdf5 format.
     HDF5 format was chosen due to large size of the output from metquest, which
@@ -196,7 +196,9 @@ class MQData(object):
             return {0: []}
         else:
             return {
-                path_len: self.__get_by_path_length(path_type, mb_id, path_len, map_back)
+                path_len: self.__get_by_path_length(
+                    path_type, mb_id, path_len, map_back
+                )
                 for path_len in path_lengths
             }
 
@@ -323,11 +325,121 @@ class MQData(object):
         return met_paths, comments
 
 
+def check_which_met_id_found(path_data: dict, *args):
+    pos_ids = path_data.keys()
+    for mb_id in args:
+        if mb_id in pos_ids:
+            return mb_id
+
+
+def get_paths_for_met(path_data: dict, met: str, len_diversity: int, name_map: dict):
+    len_diversity = min(len(path_data[met]), len_diversity)
+    path_lengths = list(path_data[met].keys())[:len_diversity]
+    # Return all paths for given path lengths (if specified)
+    # In case paths are there: length bigger than 0, otherwise return empty dict
+    if 0 in path_lengths:
+        return {0: []}
+    met_paths = {}
+    for path_len in path_lengths:
+        paths = path_data[met][path_len]
+        new_paths = []
+        for path in paths:
+            new_paths.append({name_map[r_id] for r_id in path})
+        met_paths[path_len] = new_paths
+    return met_paths
+
+
+def get_shortest_paths_data(mq_out_data: dict, metabolites: list, len_diversity=3):
+    met_paths = {}
+    comments = {}
+    all_syn = mq_out_data["all_synthesized"]
+    max_syn = mq_out_data["max_synthesized"]
+
+    for met in metabolites:
+        # Get all possible ways metquest can give ids to metabolites
+        met_tag = mq_out_data["tag"] + " " + met
+        reverse_name_map = {
+            orig_id: mq_id for mq_id, orig_id in mq_out_data["name_map"].items()
+        }
+        met_name_map = reverse_name_map.get(met, "")
+
+        # if metabolite is not in all_synthesized list, then it cannot be
+        # synthesized
+        if (
+            (met_tag not in all_syn)
+            and (met not in all_syn)
+            and (met_name_map not in all_syn)
+        ):
+            comments[met] = "can not be synthesized"
+            met_paths[met] = {}
+
+        # if metabolite is not in X_synthesized then it cannot be
+        # synthesized with max path length of X
+        elif (
+            (met_tag not in max_syn)
+            and (met not in max_syn)
+            and (met_name_map not in max_syn)
+        ):
+            comment = f"can not be synthesized with max {mq_out_data['max_paths_length']} length path"
+            comments[met] = comment
+            met_paths[met] = {}
+        else:
+            met_found = check_which_met_id_found(
+                mq_out_data["linear_paths"], met, met_tag, met_name_map
+            )
+            if met_found is not None:
+                # Check linear paths for given metabolite. Get linear and
+                # circular paths from metquest output for a given metabolite
+                linear_paths = get_paths_for_met(
+                    mq_out_data["linear_paths"],
+                    met_found,
+                    len_diversity=len_diversity,
+                    name_map=mq_out_data["name_map"],
+                )
+                if linear_paths == {0: []}:
+                    comments[met] = "Is in the medium, no need to synthesys"
+                    met_paths[met] = {}
+                elif isinstance(linear_paths, dict):
+                    comments[met] = "Found in linear paths"
+                    met_paths[met] = linear_paths
+            else:
+                c_met_found = check_which_met_id_found(
+                    mq_out_data["circular_paths"], met, met_tag, met_name_map
+                )
+                if c_met_found is not None:
+                    # Check circular paths for given metabolite
+                    circular_paths = get_paths_for_met(
+                        mq_out_data["circular_paths"],
+                        c_met_found,
+                        len_diversity=len_diversity,
+                        name_map=mq_out_data["name_map"],
+                    )
+                    if circular_paths == {0: []}:
+                        comments[met] = "Is in the medium, no need to synthesys"
+                        met_paths[met] = {}
+                    elif isinstance(circular_paths, dict):
+                        comments[met] = "Found in circular paths"
+                        met_paths[met] = circular_paths
+                else:
+                    # Otherwise check ...
+                    maybe_synthesised = all_syn - max_syn
+                    if maybe_synthesised:  # if non-empty
+                        comment = (
+                            "Problematic: can be synthesized " "but does not have paths"
+                        )
+                    else:
+                        comment = (
+                            "Maybe can not be synthesized with max "
+                            f"{mq_out_data['max_paths_length']} length path, because "
+                            "all_synthesized not bigger than max_synthesized"
+                        )
+                    comments[met] = comment
+                    met_paths[met] = {}
+    return {"paths": met_paths, "comments": comments}
+
+
 def preprocess_medium(
-    tag: str,
-    nutritional_sources: [str],
-    other_medium: [str],
-    cofactors: [str],
+    tag: str, nutritional_sources: [str], other_medium: [str], cofactors: [str],
 ):
     if cofactors is None:
         cofactors = [
@@ -406,10 +518,7 @@ def remove_cofactors_from_model(
 
 
 def run_metquest(
-    path_to_models: str,
-    seed_metabolites_tag: [str],
-    number_of_xml,
-    max_paths_length,
+    path_to_models: str, seed_metabolites_tag: [str], number_of_xml, max_paths_length,
 ):
     graph, name_map = metquest.construct_graph.create_graph(
         path_to_models, number_of_xml
@@ -418,14 +527,6 @@ def run_metquest(
     lowboundmet_pic, status_dict_pic, scope_pic = metquest.forward_pass(
         graph, seed_metabolites_tag
     )
-
-    # Number of reactions visited in individual network for the given media (seed)
-    # TODO: is this needed?
-    visited_pic = {
-        k: status_dict_pic[k]
-        for k in list(status_dict_pic)
-        if status_dict_pic[k] == "V"
-    }
 
     # BE CAREFULLY can crash computer: memory heavy and long. Better do on cluster.
     pathway_table, c_path, scope = metquest.find_pathways(
@@ -451,6 +552,8 @@ def pathsfinding(
     cofactors=None,
     max_paths_length=40,
     number_of_xml=1,
+    len_diversity=3,
+    metabolites=None,
     **kwargs,
 ):
     # Convert paths to Path objects
@@ -485,10 +588,7 @@ def pathsfinding(
 
         # Run metquest
         mq_output, graph = run_metquest(
-            tmp_dir,
-            seed_metabolites_tag,
-            number_of_xml,
-            max_paths_length,
+            tmp_dir, seed_metabolites_tag, number_of_xml, max_paths_length,
         )
 
     # Add necessary parameters to output data
@@ -496,6 +596,13 @@ def pathsfinding(
 
     # Save the output
     write_mq_data(mq_output, output_dir / "metquest.h5")
+
+    if metabolites is None:
+        metabolites = [m.id for m in model_wo_cof.metabolites]
+    shortest_paths = get_shortest_paths_data(mq_output, metabolites, len_diversity)
+
+    with open(output_dir / "shortest_paths.pkl", "wb") as fh:
+        pickle.dump(shortest_paths, fh)
 
     with open(output_dir / "graph.pkl", "wb") as fh:
         pickle.dump(graph, fh)
