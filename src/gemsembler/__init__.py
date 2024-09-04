2c5b9386e9aaebf9488148d69efcc5ad5dbdd193
Elena Matveishina <elena.matveishina@embl.de>
2024-09-04 10:56:15 +0200

    Bump version 0.8.7 -> 0.8.8

--- src/gemsembler/__init__.py
+++ src/gemsembler/__init__.py
@@ -4,7 +4,7 @@ from .creation import read_supermodel_from_pkl
 from .data import BU, LP
 from .gathering import GatheredModels, load_sbml_model
 
-__version__ = "0.8.7"
+__version__ = "0.8.8"
 
 lp_example = [
     dict(
