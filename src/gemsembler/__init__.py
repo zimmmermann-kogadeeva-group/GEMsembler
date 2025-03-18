163a849875c59dec322aa961b6a4f0acef0c22f2
Bartosz Bartmanski <bartosz.bartmanski@embl.de>
2025-03-18 15:04:23 +0100

    Bump version 0.9.10 -> 0.9.11

--- src/gemsembler/__init__.py
+++ src/gemsembler/__init__.py
@@ -4,7 +4,7 @@ from .creation import read_supermodel_from_json
 from .data import BU, LP
 from .gathering import GatheredModels, load_sbml_model
 
-__version__ = "0.9.10"
+__version__ = "0.9.11"
 
 lp_example = [
     dict(
