75f3a067714416f9786ac8648bf3c3a0bfddbf26
Bartosz Bartmanski <bartoszbartmanski@gmail.com>
2025-03-31 13:58:23 +0200

    Added docs folder and sphinx as dep.

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GEMsembler'
+copyright = '2025, Elena Matveishina, Bartosz Bartmanski'
+author = 'Elena Matveishina, Bartosz Bartmanski'
+release = '0.9.13'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
